05c1b94dbf64f8a5b1fc1e9a163b453c0e982413
Sophie Herold <sophie@hemio.de>
2023-09-18 23:12:12 +0200

    dist: Replace deps with crates.io version in lock
    Previously, the glycin-loaders tarball did not include the
    crates.io sources for glycin and glycin-utils

--- build-aux/dist-vendor.sh
+++ build-aux/dist-vendor.sh
@@ -5,8 +5,11 @@ cd "$MESON_PROJECT_DIST_ROOT"
 # Remove crates.io packaged part
 sed -i 's/"glycin",\?//' Cargo.toml
 rm -r glycin
+awk -i inplace -v RS= -v ORS='\n\n' '!/name = "glycin"/' Cargo.lock
+
 sed -i 's/"glycin-utils",\?//' Cargo.toml
 rm -r glycin-utils
+awk -i inplace -v RS= -v ORS='\n\n' '!/name = "glycin-utils"/' Cargo.lock
 
 cat Cargo.toml
 
@@ -17,6 +20,13 @@ for toml in $(find loaders/ -name Cargo.toml); do
 done
 
 sed -i "s/path = \"..\/glycin\/\"/version = \"$VERSION\"/g" tests/Cargo.toml
+
+# Trigger update in Cargo.lock
+for toml in $(find loaders/ -name Cargo.toml); do
+  cargo add --manifest-path="$toml" glycin-utils@$VERSION
+done
+
+cargo add --manifest-path=tests/Cargo.toml glycin@$VERSION
 
 # Vendor crates.io dependencies
 mkdir .cargo
